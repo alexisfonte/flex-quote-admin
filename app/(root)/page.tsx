b3a5d372360c18831a9e8ab05547d063990be584
Alexis Fonte <alexis.e.fonte@gmail.com>
2024-01-19 19:56:23 -0600

    added clerk auth

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,9 @@
-export default function Home() {
-    return (
-      <p>admin dashboard</p>
-    );
-  }
-  
+import { UserButton } from "@clerk/nextjs";
+
+export default function SetupPage() {
+  return (
+    <div className="p-4">
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
+}
